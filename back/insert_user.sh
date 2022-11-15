fafeeb6ba9c68399e7fd3fb496e1dd21d97dd906
denden <denis.c1@live.fr>
2022-11-15 15:34:00 +0100

    db insert user script

---
+++ back/insert_user.sh
@@ -0,0 +1,2 @@
+insert into users (username, email, id42, winratio, profile_pic, elo, n_win, n_lose, date_of_sign)
+values('test1', 'test1@test1.fr', '123', '50%', 'none', '1600', '1', '1', '2022-11-15')
