91fd2832cc0d0d04ebb81bc4abdcdab58848a14f
ajvar <cueille@protonmail.com>
2022-11-17 12:46:37 +0100

    adding swagger

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,10 +3,11 @@ import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as passport from 'passport';
 import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   app.setGlobalPrefix('api');
   app.enableCors({
     origin: true,
@@ -23,6 +24,14 @@ async function bootstrap() {
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(cookieParser());
+  const config = new DocumentBuilder()
+  .setTitle('Cats example')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
   await app.listen(3001);
 }
 bootstrap();
