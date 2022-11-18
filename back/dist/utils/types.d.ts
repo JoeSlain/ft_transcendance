330993a3e23db2caeb54c129b1aeb86e463ef187
Samuel Fernandez <safernan@student.42.fr>
2022-11-18 17:58:28 +0100

    delete dist

--- back/dist/utils/types.d.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "src/database/entities/User";
-export declare type Done = (err: Error, user: User) => void;
-export declare type UserDetails = {
-    username: string;
-    email: string;
-    id42: number;
-};
