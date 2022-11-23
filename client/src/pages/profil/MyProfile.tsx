8b01c205c4b719bcd4e19133883a01d4491e2a2f
ajvar <cueille@protonmail.com>
2022-11-23 18:22:24 +0100

    Automatic commit 23.11.22

--- client/src/pages/profil/MyProfile.tsx
+++ client/src/pages/profil/MyProfile.tsx
@@ -19,9 +19,8 @@ export default function MyProfile(user : userType): JSX.Element
     <>
       {user !== undefined &&
         <div className='profil flex flex-col items-center relative'>
-             <input type="text" className='text-black ' value={inputValue} onChange={handleInputChange} ></input >
- 
-        
+          <p className='text-slate-200'>Username</p>
+          <input type="text" className='text-black ' value={inputValue} onChange={handleInputChange} ></input >        
           <label className="w-64 flex justify-center items-center px-4 py-6 rounded-lg shadow-lg tracking-wide uppercase  hover:text-white">
             <img src={user.profile_pic} alt="Avatar" className='w-32 sm:w-64 avatar cursor-pointer rounded-full'/>
             <p className='avatar-txt text-xs md:text-md cursor-pointer'>Change profile picture.</p>
