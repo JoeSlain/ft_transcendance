10f467aa51045b21e0748c54ec1fc9729228af3a
ajvar <cueille@protonmail.com>
2022-12-07 16:30:47 +0100

    Added ESlint

--- client/src/services/GetUser.ts
+++ client/src/services/GetUser.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { userType } from "../types/userType";
 
 export  function getUser(id: string | undefined): Promise<userType> {
+   console.log("Fetching: " + id);
   return  axios(`http://localhost:3001/api/users/userid/${id}`, {
     withCredentials: true,
   }).then((res) => res.data);
