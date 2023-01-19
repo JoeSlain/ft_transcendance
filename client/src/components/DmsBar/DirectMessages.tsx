b95bbb737f68a9e060011e2c7ef7496c6e67cda1
denden <denis.c1@live.fr>
2023-01-19 21:53:07 +0100

    dms ongoing

---
+++ client/src/components/DmsBar/DirectMessages.tsx
@@ -0,0 +1,38 @@
+import "../../styles/chat/dms.css";
+import React, { useState } from "react";
+
+type Props = {
+  content: string;
+};
+
+const MessageContent = ({ content }: Props) => {
+  return <div className="dmContent">{content}</div>;
+};
+
+export default function DirectMessages() {
+  const [show, setShow] = useState(false);
+  const dms = [
+    {
+      from: "test1",
+      content: "sup",
+    },
+    {
+      from: "test2",
+      content: "sup",
+    },
+  ];
+  return (
+    <div className="dms">
+      {dms &&
+        dms.map((dm) => (
+          <div className="dmEntry">
+            {show && <MessageContent content={dm.content} />}
+            <div className="dmTitle" onClick={() => setShow(!show)}>
+              {" "}
+              {dm.from}{" "}
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+}
